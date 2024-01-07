f0f3ffe0425d302d9d1699961a0635ebf08723d3
ninohero98 <ninohero98@github.com>
2024-01-07 03:52:20 +0200

    5th

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
 const PORT = process.env.PORT || 3000
+
+app.use(cors())
 
 app.get('/', function (req, res) {
   res.send('Hello World')
