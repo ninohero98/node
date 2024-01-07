85cfa5414fb670cc321f9602b902be708ace4f16
ninohero98 <ninohero98@github.com>
2024-01-07 02:45:00 +0200

    2nd

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 3000
+// const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
 
-app.listen(PORT)
+app.get('/students', function (req, res) {
+    var data=[
+        {id:1, name:'nicolas'},
+        {id:2, name:'gaya'},
+        {id:3, name:'kouka'},
+        {id:4, name:'edward'}
+    ]
+
+    res.send(data)
+  })
+
+
+app.listen(3000)
